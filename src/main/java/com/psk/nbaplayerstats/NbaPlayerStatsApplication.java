6b9b3289c3e01225a3a035e6ee619730f20758a2
Beata <beata.koziel@skycash.com>
2021-10-22 23:17:14 +0200

    initial

---
+++ src/main/java/com/psk/nbaplayerstats/NbaPlayerStatsApplication.java
@@ -0,0 +1,13 @@
+package com.psk.nbaplayerstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaPlayerStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbaPlayerStatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/psk/nbaplayerstats/NbaPlayerStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psk.nbaplayerstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaPlayerStatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
